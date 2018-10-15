ff56a59c510284bca678fd907fdab6b238d472d0
Andy Lu <37600885+Andyluchina@users.noreply.github.com>
2018-10-15 16:12:55 -0400

    refactored stack....

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "stack.h"
 #define FAILED NULL
 
 typedef struct Node *TREE;
@@ -12,46 +13,6 @@ struct Node {
     char* label;
     TREE leftmostChild, rightSibling;
 };
-
-struct Stack{
-    char* stack;
-    int top;
-};
-
-struct Stack* createStack(){
-    struct Stack* s = (struct Stack*)malloc(sizeof(struct Stack));
-    s->stack = (char*)malloc(sizeof(char*));
-    s->top = -1;
-    return s;
-}
-
-void push(struct Stack* s, char c){
-    s->top++;
-    s->stack[s->top] = c;
-}
-
-char pop(struct Stack* s){
-    char temp = s->stack[s->top];
-    s->stack[s->top]= -1;
-    s->top--;
-    return temp;
-}
-
-char peek(struct Stack* s){
-   return s->stack[s->top];
-}
-
-int isEmpty(struct Stack* s){
-    if(s->top == -1){
-        return 1;
-    }
-    return 0;
-}
-
-//not sure if free method is correct
-void free_Stack(struct Stack* s){
-    free(s);
-}
 
 char* nextTerminal;
 TREE parseTree;
@@ -147,6 +108,11 @@ int main(int argc, char* args[]) {
 //    printf("s->top: %d\n", s->top);
 //    printf("isEmpty: %d\n", isEmpty(s));
 //    free_Stack(s);
+
+//    struct Stack *stack = createStack();
+//    push(stack, 'c');
+//    printf("%c\n", pop(stack));
+//    free_Stack(stack);
 
     char input[256] = "";
     while (strcmp(input, "quit") != 0) {
@@ -289,6 +255,8 @@ TREE makeNodeD(char* x){
     return root;
 }
 
+
+
 TREE makeNodeEmpty(char* label){
     TREE root = (TREE) malloc(sizeof(struct Node));
     root->label = label;
@@ -298,7 +266,7 @@ TREE makeNodeEmpty(char* label){
     return root;
 }
 
-
+//make the leaf
 TREE makeNodeChar(char* x){
     TREE root;
     root = (TREE) malloc(sizeof(struct Node));
